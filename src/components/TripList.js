01860df311b57ff9c9f03e51b0649b2b39bc51e2
Teye Michael <teyemichael@Nanas-MacBook-Pro.local>
2023-03-19 06:29:59 +0000

    adding error

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -4,7 +4,7 @@ import './TripList.css'
 export default function TripList() {
     // const [trips, setTrips] = useState([])
     const [url, setUrl] = useState('http://localhost:3000/trips')
-    const {data} = useFetch(url)
+    const {data, isPending, error} = useFetch(url)
 
     // const fetchTrips = useCallback(async ()=>{
     //     const response = await fetch(url)
@@ -20,6 +20,8 @@ export default function TripList() {
   return (
     <div className='trip-list'>
         <h2>Trip List</h2>
+        {isPending && <div>Loading trips...</div>}
+        {error && <div>{error}</div>}
         <ul>
             {data && data.map(trip=>(
                 <li key={trip.id}>
